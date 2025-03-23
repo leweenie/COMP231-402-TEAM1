b7bd04da48971c61e418e166f3e54bb1d4ce4481
Mohamed Meghji <101439085@georgebrown.ca>
2025-03-23 03:25:14 +0300

    schema issue fixed.

--- server/routes/applicationRoutes.js
+++ server/routes/applicationRoutes.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const submitApplication = require("../controllers/applicationController")
-
+const { submitApplication } = require("../controllers/applicationController"); 
 
 router.post("/apply", submitApplication);
-
 
 module.exports = router;
