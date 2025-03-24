24062eede3b17d41fdc8809734ba017c00bb159c
Natalie Rekai <nrekai@my.centennialcollege.ca>
2025-03-24 02:42:29 -0400

    header img links to home

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
    <Container className='p-2 banner'>
       <Row className='p-2'>
-         <Col xs={12} md={6} className='p-2 image'><img src='/src/assets/HeroForHire_banner.png' /></Col>
+         <Col xs={12} md={6} className='p-2 image'><a href="/" ><img src='/src/assets/HeroForHire_banner.png' /></a></Col>
          <Col xs={12} md={6} className='p-2 title'><h1>A <em>Super</em> Hero Can Solve<br/>Your <em>Super</em> Problems</h1></Col>
       </Row>
     </Container>
