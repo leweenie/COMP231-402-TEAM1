354cc85b5490e4032a1a2cac190058a3f2155213
Natalie Rekai <nrekai@my.centennialcollege.ca>
2025-04-05 13:08:42 -0400

    View applicant profile - page and button link

---
+++ client/src/components/DisplayApplicantProfile.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useParams, Link } from "react-router";
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import UserProfile from './UserProfile';
+
+const DisplayApplicantProfile = () => {
+   const applicantId = useParams().id
+   return (
+      <>
+         <Container fluid className='p-2 return-outer d-flex justify-content-center'>
+            <Row className='p-2 return-inner flex-grow-1'>
+               <Col>
+                  <Link to={'/dashboard'}>&larr; Return to Dashboard</Link>
+               </Col>
+            </Row>
+         </Container>
+         <Container className='p-2'>
+            <Row className='p-2'>
+               <Col xs={12}>
+                  <UserProfile userId={applicantId}/>
+               </Col>
+            </Row>
+         </Container>
+      </>
+   )
+};
+
+export default DisplayApplicantProfile;
+
